4b9723d21c6f4bffe12b974e4b57455c835dafdf
zengmingjie <zengmingjie@yy.com>
2023-01-05 23:57:28 +0800

    day13

---
+++ Day13-进程和线程/练习2.py
@@ -0,0 +1,45 @@
+'''
+例子2：使用多进程对复杂任务进行“分而治之”。
+我们来完成1~100000000求和的计算密集型任务，这个问题本身非常简单，有点循环的知识就能解决，代码如下所示。
+
+队列：先进先出
+'''
+
+from multiprocessing import Process, Queue
+from random import randint
+from time import time
+
+
+def task_handler(curr_list, result_queue):
+    total = 0
+    for number in curr_list:
+        total += number
+    result_queue.put(total)
+
+
+def main():
+    processes = []
+    number_list = [x for x in range(1, 100000001)]
+    result_queue = Queue()
+    index = 0
+    # 启动8个进程将数据切片后进行运行
+    for _ in range(8):
+        p = Process(target=task_handler, args=(number_list[index:index + 12500000], result_queue))
+        index += 12500000
+        processes.append(p)
+        p.start()
+    # 开始记录所有进程执行完成花费的时间
+    start = time()
+    for p in processes:
+        p.join()
+    # 合并执行结果
+    total = 0
+    while not result_queue.empty():
+        total += result_queue.get()
+    print(total)
+    end = time()
+    print('Execution time: ', (end - start), 's', sep='')
+
+
+if __name__ == '__main__':
+    main()
